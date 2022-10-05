e8ba20c7dca66ff479123ce0f5be7549310a8bb7
Patrick Zimmerer <p.zimmerer@east-young.de>
2022-10-05 16:02:36 +0200

    added nuxi, axios and scrollbar styling

--- app.vue
+++ app.vue
@@ -13,4 +13,19 @@ body {
 	background-color: black;
 	height: 100%;
 }
+/*  BOSCH CUSTOM SCROLLBAR   */
+::-webkit-scrollbar {
+	width: 0.25rem;
+	height: 0;
+}
+
+::-webkit-scrollbar-track {
+	background-color: rgba(0, 0, 0, 0.4);
+	border-radius: 0.625rem;
+}
+
+::-webkit-scrollbar-thumb {
+	background-color: #dfdfdf;
+	border-radius: 0.625rem;
+}
 </style>
